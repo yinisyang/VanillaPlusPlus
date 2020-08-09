5a900c13caf7d763a659a7879e37664a6dca892f
yinisyang <dyllonbow@yahoo.com>
2020-08-08 19:28:12 -0700

    Fixed crash in Prod
    I was doing a big dumb and not using mixins correctly. This was causing the game to crash in a non-dev environment when trying to melt metal blocks.

--- src/main/java/vanillaplusplus/mixin/BlocksMixin.java
+++ src/main/java/vanillaplusplus/mixin/BlocksMixin.java
@@ -8,19 +8,21 @@ import net.minecraft.block.MaterialColor;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import vanillaplusplus.blocks.meltable.CustomGoldBlock;
 import vanillaplusplus.blocks.meltable.CustomIronBlock;
 
 @Mixin(Blocks.class)
 public class BlocksMixin {
 
-    private static Block register(String id, Block block) {
+    @Inject(method="register", at=@At("HEAD"), cancellable = true)
+    private static void onRegister(String id, Block block, CallbackInfoReturnable<Block> cbi) {
         if (id.equals("iron_block")) {
-            return (Block)Registry.register(Registry.BLOCK, id, new CustomIronBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).requiresTool().strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL)));
+            cbi.setReturnValue(Registry.register(Registry.BLOCK, id, new CustomIronBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.IRON).requiresTool().strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL))));
         } else if (id.equals("gold_block")) {
-            return (Block)Registry.register(Registry.BLOCK, id, new CustomGoldBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.GOLD).requiresTool().strength(3.0F, 6.0F).sounds(BlockSoundGroup.METAL)));
+            cbi.setReturnValue(Registry.register(Registry.BLOCK, id, new CustomGoldBlock(FabricBlockSettings.of(Material.METAL, MaterialColor.GOLD).requiresTool().strength(3.0F, 6.0F).sounds(BlockSoundGroup.METAL))));
         }
-
-        return (Block)Registry.register(Registry.BLOCK, id, block);
     }
 }
