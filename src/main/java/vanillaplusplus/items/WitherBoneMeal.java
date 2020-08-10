db20f036f3943d6d0215290fb5eeedc5be09f7d5
yinisyang <dyllonbow@yahoo.com>
2020-08-09 17:35:49 -0700

    Fixed Wither Bone Meal Crash
    Fixed crash when player used wither bone meal on blocks besides nether wart
    Change wither bone meal to affect nether wart in a 3x3 area

--- src/main/java/vanillaplusplus/items/WitherBoneMeal.java
+++ src/main/java/vanillaplusplus/items/WitherBoneMeal.java
@@ -8,6 +8,7 @@ import net.minecraft.item.ItemUsageContext;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
 
 import java.util.Random;
@@ -21,27 +22,44 @@ public class WitherBoneMeal extends Item {
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
         World world = context.getWorld();
+        BlockState centerBlockState = world.getBlockState(context.getBlockPos());
 
-        BlockPos pos = context.getBlockPos();
+        if (centerBlockState.getBlock().is(Blocks.NETHER_WART)) {
+            // Try to apply the growth affect in a 3x3 area center on the
+            // used block
+            boolean didGrow = false;
+            for (int i  = -1; i <= 1; i++) {
+                for (int j = -1; j <= 1; j++) {
+                    BlockPos pos = context.getBlockPos().add(new Vec3i(i, 0, j));
+                    BlockState blockState = world.getBlockState(pos);
 
-        BlockState blockState = world.getBlockState(pos);
-        int age = blockState.get(NetherWartBlock.AGE);
-        int increment = new Random().nextInt(2) + 1;
+                    if (blockState.getBlock().is(Blocks.NETHER_WART)) {
+                        int age = blockState.get(NetherWartBlock.AGE);
+                        int increment = new Random().nextInt(2) + 1;
 
-        if (age + increment > 3) {
-            increment = 3 - age;
-        }
+                        if (age + increment > 3) {
+                            increment = 3 - age;
+                        }
 
-        if (blockState.getBlock() == Blocks.NETHER_WART && age < 3) {
+                        if (blockState.getBlock() == Blocks.NETHER_WART && age < 3) {
+                            didGrow = true;
+                            if (!world.isClient()) {
+                                blockState = blockState.with(NetherWartBlock.AGE, age + increment);
+                                world.setBlockState(pos, blockState, 2);
+                            } else {
+                                world.addParticle(ParticleTypes.HAPPY_VILLAGER, (double) pos.getX() + 0.5D, (double) pos.getY() + 0.5D, (double) pos.getZ() + 0.5D, 0.0D, 0.0D, 0.0D);
+                            }
 
-            if (!world.isClient()) {
-                blockState = blockState.with(NetherWartBlock.AGE, age + increment);
-                world.setBlockState(pos, blockState, 2);
-            } else {
-                world.addParticle(ParticleTypes.HAPPY_VILLAGER, (double) pos.getX() + 0.5D, (double) pos.getY() + 0.5D, (double) pos.getZ() + 0.5D, 0.0D, 0.0D, 0.0D);
+                        }
+                    }
+                }
             }
-            context.getStack().decrement(1);
+
+            if (didGrow) {
+                context.getStack().decrement(1);
+                return ActionResult.SUCCESS;
+            }
         }
-        return ActionResult.SUCCESS;
+        return ActionResult.PASS;
     }
 }
